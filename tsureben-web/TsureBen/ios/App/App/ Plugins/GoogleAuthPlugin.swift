f2e31548394ec4cfafbe39831930678077c3a352
伊奈 洋 <sky.voltric424@gmail.com>
2025-11-26 09:05:09 +0900

    ディレクトリ最適化

---
+++ tsureben-web/TsureBen/ios/App/App/ Plugins/GoogleAuthPlugin.swift
@@ -0,0 +1,8 @@
+//
+//  GoogleAuthPlugin.swift
+//  App
+//
+//  Created by 伊奈洋 on 2025/07/31.
+//
+
+import Foundation
---
+++ tsureben-web/TsureBen/ios/App/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  App
+//
+//  Created by 伊奈洋 on 2025/07/30.
+//
+
+import Foundation
